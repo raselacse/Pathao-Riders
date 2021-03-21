6fb5c0bcd235a68d2812ece5d3db971087ed9925
Md. Rasel Hossain <rasel.as.cse@gmail.com>
2021-03-21 23:12:26 +0600

    can't show the selected car in a dynamic way

--- src/components/Destination/Destination.js
+++ src/components/Destination/Destination.js
@@ -1,17 +1,20 @@
 import React, { useContext } from 'react';
-import { Col, Container, Form, Nav, Navbar, Row } from 'react-bootstrap';
+import { Col, Container, Nav, Navbar, Row } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 import { UserContext } from '../../App';
 import LoggedInHeader from '../LoggedInHeader/LoggedInHeader';
 import './Destination.css'
 import logo from '../../images/pathao.png';
+import fakeData from '../../fakeData';
+import peopleIcon from '../../images/peopleicon.png'
 
 
-
-const Destination = () => {   
+const Destination = () => {
     const user = useContext(UserContext)
-    console.log(user)
-    const {displayName} = user[0];
+    const { displayName } = user[0];
+
+    const url = fakeData;
+    console.log(url[0].name)
 
     return (
         <div>
@@ -37,35 +40,45 @@ const Destination = () => {
             </Container>
             <Container style={{ marginTop: '50px' }}>
                 <Row>
-                    <Col xl={2}>
-                        <Form>
-                            <Form.Group>
-                                <Form.Label>Origin</Form.Label>
-                                <Form.Control style={{ backgroundColor: 'rgba(230, 230, 230, 0.616)' }} type="text" placeholder="Gazipur" />
-                            </Form.Group>
+                    <Col xl={3}>
+                        <div className="destination-content">
+                            <div className="destination">
+                                <h4>Mirpur</h4>
+                                <h4>Dhanmondi</h4>
+                            </div>
+                            <div className="rideInfo">
+                                <img src={url[0].image} alt="" className="car" />
+                                <h6>Car</h6>
+                                <img src={peopleIcon} alt="" className="peopleIcon" />
+                                <h6>4</h6>
+                                <h6>$67</h6>
+                            </div>
+                            <div className="rideInfo">
+                                <img src={url[1].image} alt="" className="car" />
+                                <h6>Car</h6>
+                                <img src={peopleIcon} alt="" className="peopleIcon" />
+                                <h6>4</h6>
+                                <h6>$67</h6>
+                            </div>
+                            <div className="rideInfo">
+                                <img src={url[2].image} alt="" className="car" />
+                                <h6>Car</h6>
+                                <img src={peopleIcon} alt="" className="peopleIcon" />
+                                <h6>4</h6>
+                                <h6>$67</h6>
+                            </div>
+                            <div className="rideInfo">
+                                <img src={url[3].image} alt="" className="car" />
+                                <h6>Car</h6>
+                                <img src={peopleIcon} alt="" className="peopleIcon" />
+                                <h6>4</h6>
+                                <h6>$67</h6>
+                            </div>
+                        </div>
 
-                            <Form.Group>
-                                <Form.Label>Destination</Form.Label>
-                                <Form.Control style={{ backgroundColor: 'rgba(230, 230, 230, 0.616)' }} type="text" placeholder="Savar" />
-                            </Form.Group>
-                            <Row>
-                                <Col>
-                                    <Form.Group>
-                                        <Form.Label>From</Form.Label>
-                                        <Form.Control style={{ backgroundColor: 'rgba(230, 230, 230, 0.616)' }} type="date" />
-                                    </Form.Group>
-                                </Col>
-                                <Col>
-                                    <Form.Group>
-                                        <Form.Label>To</Form.Label>
-                                        <Form.Control style={{ backgroundColor: 'rgba(230, 230, 230, 0.616)' }} type="date" />
-                                    </Form.Group>
-                                </Col>
-                            </Row>
-                        </Form>
                     </Col>
 
-                    <Col xl={10}>
+                    <Col xl={9}>
                         <div className="google-map">
                             <iframe className="w-100 h-100" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d7496149.953730211!2d85.8462125075647!3d23.452185887261443!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x30adaaed80e18ba7%3A0xf2d28e0c4e1fc6b!2sBangladesh!5e0!3m2!1sen!2sbd!4v1600418722974!5m2!1sen!2sbd"></iframe>
                         </div>
